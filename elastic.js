85967e7ac56bfb89b891322be030b64980a5b22c
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-24 22:02:53 +0530

    index mapping added

--- elastic.js
+++ elastic.js
@@ -8,10 +8,10 @@ const elasticClient = new elasticsearch.Client({
 
 module.exports = {
 
-	ping : (req,res) +> {
+	ping : (req,res) => {
 			elasticClient.ping({
 				requestTimeout : 30000,
-			},function (error) {
+			}, (error) => {
 			   if(error){
 			   	res.status(500)
 			   		return res.json({status : false, msg: 'elasticsearch cluster is down'})
@@ -21,7 +21,32 @@ module.exports = {
 			   		return res.json({status : true, msg: 'Sucess! elasticsearch cluster is up'})
 			   }
 			})
-	}
+	},
+
+	initIndex :  (req,res, indexName) => {
+		 elasticClient.indices.create({
+		 	 index : indexName
+		 }).then( (response) => {
+		 	 res.status(200)
+		 	 return res.json(response);
+		 }).catch((err) => {
+		 	res.status(500)
+		 	return res.json(err);
+		 })
+	},
+
+	initExists:  (req,res, indexName) => {
+		 elasticClient.indices.exists({
+		 	 index : indexName
+		 }).then( (response) => {
+		 	 res.status(200)
+		 	 return res.json(response);
+		 }).catch((err) => {
+		 	res.status(500)
+		 	return res.json(err);
+		 })
+	},
+
 
 
 }
